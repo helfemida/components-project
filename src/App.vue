c8064701ce790d40bf7b4c03cf86e0605de7c13b
Albina Yermek <yermekalbina@gmail.com>
2023-12-23 21:12:06 +0600

    initial commit (header)

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <Header></Header>
+</template>
+  
+<script>
+    import Header from './components/Header.vue';
+  
+    export default {
+        name: 'App',
+        components: {
+        Header
+        }
+    }
+</script>
+  
+<style>
+    *{
+        box-sizing: border-box;
+        padding: 0;
+        margin:0;
+    }
+    body{
+        color: navy;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+</style>
+  
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container">
+        <div class="left-side">
+            <AppLogo></AppLogo>
+            <SearchBar></SearchBar>
+        </div>
+
+        <div class="right-side">
+            <LanguageButton></LanguageButton>
+            <img :src="require('@/assets/settings.png')" alt="">
+            <img :src="require('@/assets/megaphone.png')" alt="">
+            <HeaderAccount></HeaderAccount>
+        </div> 
+    </div>
+</template>
+
+<script>
+    import AppLogo from './AppLogo.vue'
+    import SearchBar from './SearchBar.vue';
+    import LanguageButton from './LanguageButton.vue';
+    import HeaderAccount from './HeaderAccount.vue';
+
+    export default {
+        name: 'App',
+        components: {
+        AppLogo,
+        SearchBar,
+        LanguageButton,
+        HeaderAccount
+        }
+    }
+</script>
+
+<style>
+    .container{
+        width: 100vw;
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+        margin: 1% 2% 1% 2%;
+        align-items: center;
+    }
+    .left-side{
+        display: flex;
+        width: 600px;
+        justify-content: space-between;
+
+    }
+    .right-side{
+        display: flex;
+        width: 520px;
+        justify-content: space-between;
+        align-items: center;
+    }
+    img{
+        width: 20px;
+        height: 20px;
+    }
+</style>
+
+  
