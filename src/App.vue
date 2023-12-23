7d19741c11fa4a1c5d7b7ff8f26bc71039161195
Albina Yermek <yermekalbina@gmail.com>
2023-12-23 23:10:35 +0600

    Menu added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
     <Header></Header>
+    <MenuBar></MenuBar>
 </template>
   
 <script>
     import Header from './components/Header.vue';
-  
+    import MenuBar from './components/MenuBar.vue';
+
     export default {
         name: 'App',
         components: {
-        Header
+        Header,
+        MenuBar
         }
     }
 </script>
@@ -23,5 +26,6 @@
         color: navy;
         font-family: Verdana, Geneva, Tahoma, sans-serif;
     }
+    
 </style>
   
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="menu-bar">
+        <MenuItem v-for="(item, index) in elements" :key="index" :itemName="item.itemName" :imageName="item.imageName"></MenuItem>
+    </div>
+</template>
+
+<script scoped>
+    import MenuItem from './MenuItem.vue';
+
+    export default{
+        components:{
+            MenuItem
+        },
+        data(){
+            return{
+                elements: [
+                    {itemName: 'Dashboard', imageName: 'Item 1.png'},
+                    {itemName: 'Email', imageName: 'Item 1.png'},
+                    {itemName: 'Crypto', imageName: 'Item 1.png'},
+                    {itemName: 'Kanban', imageName: 'Item 1.png'},
+                    {itemName: 'Invoicing', imageName: 'Item 1.png'},
+                    {itemName: 'Banking', imageName: 'Item 1.png'},
+                    {itemName: 'File Manager', imageName: 'Item 1.png'},
+                    {itemName: 'User', imageName: 'Item 1.png'},
+                    {itemName: 'Calendar', imageName: 'Item 1.png'},
+                ]
+            };
+        }
+    }
+</script>
